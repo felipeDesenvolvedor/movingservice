19d6057129c4fe742d0fe8644686c3c333e1671a
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-02-14 00:41:22 -0300

    Config inicial do projeto

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin');
+
+var saaspath = "build/sass/*.scss";    
+var csspath  = "assets/css";
+
+
+gulp.task('watch', function(){
+  gulp.watch(saaspath).on('change', function() {
+    gulp.src(saaspath)
+    .pipe(sass())
+    .pipe(cssmin())
+    .pipe(gulp.dest(csspath));
+  });
+  
+});
+
+gulp.task('sass', function () {
+  gulp.src(saaspath)
+  .pipe(sass())
+  .pipe(cssmin())
+  .pipe(gulp.dest(csspath));
+});
+
+gulp.task('default', gulp.series ('watch', gulp.parallel('sass'),
+
+    function (done) { done(); }    
+));
